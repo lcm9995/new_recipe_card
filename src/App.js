edf8eac6fc06e0fe50aca37eeded06395593c65c
lcm9995 <lcm9995@nyu.edu>
2025-09-10 12:30:28 -0400

    new initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+    return (
+        <><main>
+            <div>App</div>
+        </main></>
+    )
+}
+export default App 
+
+//#whenever we say export default, we don't need curly braces when we import that component 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './global.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>) // this block is the bare minimum to inject react app into an html file
